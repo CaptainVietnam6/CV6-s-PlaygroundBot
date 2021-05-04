33d0599257495688ec6c213c90ba760f55a1b5c9
CaptainVietnam6 <kiet.riley2005@gmail.com>
2021-05-04 09:43:35 -1000

    commit test final????????

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ Copyright (c) 2021 Kiet Pham <kiet.riley2005@gmail.com>
 This software/program has a copyright license, more information is in the 'LICENSE' file
 IF YOU WANT TO USE/COPY/MODIFY/REPRODUCE/RE-DISTRIBUTE THIS PROGRAM, YOU MUST INCLUDE A COPY OF THE LICENSE
 
-Author Name: Kiet P.
+Author Name: Kiet Pham
 Author Contact: kiet.riley2005@gmail.com
 Discord: CaptainVietnam6#0001
 Discord Server: https://discord.gg/3z76p8H5yj
 GitHub: https://github.com/CaptainVietnam6
 Instagram: @itz_kietttttttttt
-Program Status: ACTIVE
+Program Status: ACTIVE - BETA
 '''
 
 #imports related to discord or discord packages
